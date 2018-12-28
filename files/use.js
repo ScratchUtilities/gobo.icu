ffd53f67547b1c089baad7f2eae59dfadbbed542
Russell <you@example.com>
2018-12-28 11:39:10 +0100

    make output use a link and speed up base64 conversions

--- files/use.js
+++ files/use.js
@@ -1,10 +1,40 @@
-function fromB64(value) {
+function fromB64(string) {
     const chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_";
-    num = 0;
-    track = 0;
-    while (track<value.length) {
-	num = (num << 6) | chars.indexOf(value[track]);
-	track ++;
+    var octal = "";
+    var digit;
+    for (var index = 0; index < string.length; index ++) {
+	digit = chars.indexOf(string[index]);
+	octal += chars[digit >> 3] + chars[digit & 7]
     }
-    return num;
+    return parseInt(octal, 8);
+}
+
+function project() {
+    var code = String(window.location).split('#')[1];// gobo.cf/p#<id>
+    var projectid = fromB64(code);
+    window.location.replace("https://scratch.mit.edu/projects/" + projecct + "/");
+}
+function editor() {
+    var code = String(window.location).split('#')[1];// gobo.cf/p/e#<id>
+    var projectid = fromB64(code);
+    window.location.replace("https://scratch.mit.edu/projects/" + projectid + "/#editor");
+}
+function topic() {
+    var code = String(window.location).split('#')[1];// gobo.cf/d#<id>
+    var topicid = fromB64(code);
+    window.location.replace("https://scratch.mit.edu/discuss/topic/" + topicid + "/");
+}
+function studio() {
+    var code = String(window.location).split('#')[1];// gobo.cf/s#<id>
+    var studioid = fromB64(code);
+    window.location.replace("https://scratch.mit.edu/studios/" + studioid + "/");
+}
+function topic() {
+    var code = String(window.location).split('#')[1];// gobo.cf/d#<id>
+    var topicid = fromB64(code);
+    window.location.replace("https://scratch.mit.edu/discuss/topic/" + topicid + "/");
+}
+function user() {
+    var code = String(window.location).split('#')[1];// gobo.cf/u#<username>
+    window.location.replace("https://scratch.mit.edu/users/" + code + "/");
 }
