52fbfe1bddf8dcb75e5857b770020dac135db2ae
Russell <you@example.com>
2019-03-05 18:24:16 +0100

    pretify files/longUrl.js

--- files/longUrl.js
+++ files/longUrl.js
@@ -1,28 +1,28 @@
 function longUrl(hash) {
-  var urlStarts = {
-    u: "https://scratch.mit.edu/users/",
-    p: "https://scratch.mit.edu/projects/",
-    e: "editor",
-    s: "https://scratch.mit.edu/studios/",
-    d: "https://scratch.mit.edu/discuss/topic/"
-  };
-  var type = urlStarts[hash[1]];
-  if (type == "https://scratch.mit.edu/users/") {
-    return type + hash.substring(2);
-  } else {
-    if (type == "editor") {
-      return (
-        "https://scratch.mit.edu/projects/" +
-        base64.decode(hash.substring(2)) +
-        "/editor"
-      );
+    var urlStarts = {
+	u: "https://scratch.mit.edu/users/",
+	p: "https://scratch.mit.edu/projects/",
+	e: "editor",
+	s: "https://scratch.mit.edu/studios/",
+	d: "https://scratch.mit.edu/discuss/topic/"
+    };
+    var type = urlStarts[hash[1]];
+    if (type == "https://scratch.mit.edu/users/") {
+	return type + hash.substring(2);
     } else {
-      return type + base64.decode(hash.substring(2));
+	if (type == "editor") {
+	    return (
+		"https://scratch.mit.edu/projects/" +
+		    base64.decode(hash.substring(2)) +
+		    "/editor"
+	    );
+	} else {
+	    return type + base64.decode(hash.substring(2));
+	}
     }
-  }
 }
 if (window.location.hash === "") {
-  window.location.replace("/home/");
+    window.location.replace("/home/");
 } else {
-  window.location.replace(longUrl(window.location.hash));
+    window.location.replace(longUrl(window.location.hash));
 }
