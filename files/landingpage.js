086c1d7dba7e3c5846eb044d7830817de0458d06
Russell <you@example.com>
2019-03-08 08:16:00 +0100

    preview page

--- files/landingpage.js
+++ files/landingpage.js
@@ -2,3 +2,33 @@ function moveOut() {
   document.getElementById("landingPage").classList.add("move-out");
   document.getElementById("fillinPage").classList.add("move-in");
 }
+
+function updatePreview() {
+    setCookie("preview", document.getElementById("preview").checked, 3652.5);
+}
+
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  var expires = "expires="+d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "false";
+}
+
+function setup() {
+    document.getElementById("preview").checked = getCookie("preview");
+}
